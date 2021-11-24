8a475ff928926f0e42541aa2a81affb228e07456
VWaghray <87341415+VWaghray@users.noreply.github.com>
2021-11-24 17:43:43 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,21 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDhT4CWBXWIbc71OGATJYDoJLx2eI6oYrc",
+  authDomain: "chatting-c2959.firebaseapp.com",
+  databaseURL: "https://chatting-c2959-default-rtdb.firebaseio.com",
+  projectId: "chatting-c2959",
+  storageBucket: "chatting-c2959.appspot.com",
+  messagingSenderId: "39406088982",
+  appId: "1:39406088982:web:60d75349fa12a5f0971637"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
